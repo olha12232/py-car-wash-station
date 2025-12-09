55e8218efb038cfcd339dfb8c0e6dfe324d6649c
Andrii Bovsunovskyi <an.bvsn@gmail.com>
2025-12-09 22:35:21 +0200

    Bump Python to 3.14

--- app/main.py
+++ app/main.py
@@ -1,8 +1,56 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str,
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        # формула: клас * різниця брудності * рейтинг / відстань
+        dirt_difference = self.clean_power - car.clean_mark
+        price = (
+            car.comfort_class
+            * dirt_difference
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(price, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        # піднімаємо чистоту машини до рівня мийки
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def serve_cars(self, cars: list) -> float:
+        total_income = 0.0
+
+        for car in cars:
+            if car.clean_mark < self.clean_power:   # машина брудніша за можливості мийки
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return round(total_income, 1)
+
+    def rate_service(self, new_rate: float) -> None:
+        # нова середня = (стара сума + новий рейтинг) / нова кількість
+        total = self.average_rating * self.count_of_ratings + new_rate
+        self.count_of_ratings += 1
+        self.average_rating = round(total / self.count_of_ratings, 1)
